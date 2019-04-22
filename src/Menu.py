f5e6be95f5c7171dc314345dfe17da0ecd7de026
felipebomfim <felipebomfim4161@gmail.com>
2019-04-22 23:22:13 +0100

    Adicionei Menu

--- src/Menu.py
+++ src/Menu.py
@@ -7,14 +7,18 @@ class Menu:
         inMenu = True
         clock = pygame.time.Clock()
         #bg = pygame.image.load('static/Images/Background/Menu_Bgnd.png')
+        #D = pygame.image.load('static/Images/Degrees/D.png')
+        #D = pygame.transform.scale(D, (70,70))
+        scr_width, scr_height = (pygame.display.get_surface()).get_size()
         while inMenu:
             clock.tick(10)
             screen.fill((0,0,0))
-            #screen.blit(bg, (0,0))
-            #self.message_to_screen("Use arrows up and down, and enter to select ", y_delta = -200, color = (255, 255, 255))
-            self.blockList = [Menu.Retangulo(screen,"Start",-100)
-                    ,Menu.Retangulo(screen,"Best Scores",0)
-                    ,Menu.Retangulo(screen,"Exit",100)]
+            self.message_to_screen("Gagálaga",y_delta = -200)
+            #screen.blit(bg, (0,-100))
+            #screen.blit(D, (scr_width/2-20,70))
+            self.blockList = [Menu.Retangulo(screen,"Start",0)
+                    ,Menu.Retangulo(screen,"Best Scores",100)
+                    ,Menu.Retangulo(screen,"Exit",200)]
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -45,14 +49,14 @@ class Menu:
             pygame.draw.rect(screen, (0,200,0), (selected.x, selected.y, selected.width, selected.height),7)
             pygame.display.update()
 
-    def message_to_screen(self, text, y_delta=0, color = (0,0,0)):
+    def message_to_screen(self, text, y_delta=0, color = (226,161,9)):
         textSurf, textRect = self.text_objects(text,color)
         textRect.center = self.screen.get_width()/2 , self.screen.get_height()/2+y_delta
         self.screen.blit (textSurf, textRect)
 
     def text_objects(self, text , color):
-            font = pygame.font.SysFont('Comic Sans', 50)
-            textSurface = font.render(text, True , (0,0,0))
+            font = pygame.font.SysFont('Arial Black', 80)
+            textSurface = font.render(text, True , color)
             return textSurface, textSurface.get_rect()
     
     class Retangulo:
@@ -74,12 +78,3 @@ class Menu:
             textSurf, textRect = self.text_objects(text,color)
             textRect.center = self.screen.get_width()/2 , self.screen.get_height()/2+y_delta
             self.screen.blit (textSurf, textRect)
-"""
-pygame.init()
-screen = pygame.display.set_mode((750,800))
-pygame.display.set_caption("Menu")
-menuu = Menu(screen)
-pygame.time.delay(1000)
-pygame.quit()
-quit()
-"""
