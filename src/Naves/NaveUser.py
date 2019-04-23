ef67c6424f7d1a9484da7b797505b7e6c752bb58
joao <joaocfariaf@gmail.com>
2019-04-22 21:53:52 -0300

    Nave in the screen

--- src/Naves/NaveUser.py
+++ src/Naves/NaveUser.py
@@ -27,3 +27,18 @@ class NaveUser(Nave):
     def shooting(self):
         shoot = Shot((self._position[0] + 50, self._position[1]), (0, -100), self._screen)
         return shoot
+
+    def updates_position(self, delta_t):
+        width, height = pygame.display.get_surface().get_size()
+        self._position = (self._velocity[0] * delta_t + self._position[0],
+                          self._velocity[1] * delta_t + self._position[1])
+
+        if (self._position[0] < -self._size[0]/2 ):
+            self._position = (-self._size[0]/2, self._position[1])
+        elif (self._position[0] + self._size[0]/2 > width):
+            self._position = (width - self._size[0]/2, self._position[1])
+        
+        if(self._position[1] < -self._size[1]/2): 
+            self._position = (self._position[0], -self._size[1]/2)
+        elif(self._position[1] + self._size[1]/2 > height):
+            self._position = (self._position[0], height - self._size[1]/2)
